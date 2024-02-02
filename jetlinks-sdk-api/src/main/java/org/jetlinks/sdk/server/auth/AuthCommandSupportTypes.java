a0b3f933f27e084a2f3446b9d186812a50950cc3
zhangji <125540670@qq.com>
2024-02-02 17:46:43 +0800

    feat(SDK): 添加用户和组织的查询命令

---
+++ jetlinks-sdk-api/src/main/java/org/jetlinks/sdk/server/auth/AuthCommandSupportTypes.java
@@ -0,0 +1,17 @@
+package org.jetlinks.sdk.server.auth;
+
+/**
+ * 权限命令类型定义.
+ *
+ * @author zhangji 2024/2/2
+ * @since 1.0.0
+ */
+public interface AuthCommandSupportTypes {
+
+    // 用户
+    String user = "user";
+
+    // 组织
+    String organization = "organization";
+
+}
