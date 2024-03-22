ebb5197df8ac3dd5975e4ab14361a16d50a9536b
liusq <106655480+liu4410@users.noreply.github.com>
2024-03-22 16:54:10 +0800

    feat(sdk): 增加基础命令 (#9)
    * feat(sdk): 增加基础命令
    
    * fix(sdk): 优化基础命令
    
    * fix(sdk): 优化基础命令
    
    * fix(sdk): 优化

---
+++ jetlinks-sdk-api/src/main/java/org/jetlinks/sdk/server/commons/cmd/DisabledCommand.java
@@ -0,0 +1,37 @@
+package org.jetlinks.sdk.server.commons.cmd;
+
+import org.jetlinks.core.command.CommandHandler;
+import org.jetlinks.core.command.CommandUtils;
+import org.jetlinks.core.metadata.SimpleFunctionMetadata;
+import org.jetlinks.core.metadata.SimplePropertyMetadata;
+import org.jetlinks.core.metadata.types.ArrayType;
+import org.jetlinks.core.metadata.types.StringType;
+import reactor.core.publisher.Mono;
+
+import java.util.Collections;
+import java.util.function.Function;
+
+/**
+ * @author liusq
+ * @date 2024/3/21
+ */
+public class DisabledCommand extends EnabledCommand {
+
+    public static CommandHandler<EnabledCommand, Mono<Void>> createHandler(Function<EnabledCommand, Mono<Void>> handler) {
+
+        return CommandHandler.of(
+                () -> {
+                    SimpleFunctionMetadata metadata = new SimpleFunctionMetadata();
+                    metadata.setId(CommandUtils.getCommandIdByType(DisabledCommand.class));
+                    metadata.setName("禁用");
+                    metadata.setDescription("禁用或注销");
+                    metadata.setInputs(Collections.singletonList(SimplePropertyMetadata.of("idList", "id数组",
+                                                                                           new ArrayType())));
+
+                    return metadata;
+                },
+                (cmd, ignore) -> handler.apply(cmd),
+                DisabledCommand::new
+        );
+    }
+}
