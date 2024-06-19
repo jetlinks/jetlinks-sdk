59342084b0c4f1f94f1dccfab30282a183dd5428
zhouhao <zh.sqy@qq.com>
2024-06-19 09:55:49 +0800

    refactor: 优化命令

--- jetlinks-sdk-api/src/main/java/org/jetlinks/sdk/server/media/StartPushStreamingCommand.java
+++ jetlinks-sdk-api/src/main/java/org/jetlinks/sdk/server/media/StartPushStreamingCommand.java
@@ -1,11 +1,15 @@
 package org.jetlinks.sdk.server.media;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.jetlinks.core.command.AbstractCommand;
+import org.jetlinks.core.command.CommandMetadataResolver;
+import org.jetlinks.core.metadata.FunctionMetadata;
 import org.jetlinks.sdk.server.utils.CastUtils;
 import reactor.core.publisher.Mono;
 
 public class StartPushStreamingCommand extends AbstractCommand<Mono<MediaStreamInfo>, StartPushStreamingCommand> {
 
+    @Schema(description = "设备ID")
     public String getDeviceId() {
         return getOrNull("deviceId", String.class);
     }
@@ -14,6 +18,7 @@ public class StartPushStreamingCommand extends AbstractCommand<Mono<MediaStreamI
         return with("deviceId", deviceId);
     }
 
+    @Schema(description = "通道ID")
     public String getChannelId() {
         return getOrNull("channelId", String.class);
     }
@@ -22,6 +27,7 @@ public class StartPushStreamingCommand extends AbstractCommand<Mono<MediaStreamI
         return with("channelId", channelId);
     }
 
+    @Schema(description = "流ID")
     public String getStreamId() {
         return getOrNull("streamId", String.class);
     }
@@ -30,6 +36,8 @@ public class StartPushStreamingCommand extends AbstractCommand<Mono<MediaStreamI
         return with("streamId", streamId);
     }
 
+    //当指定了这个参数时,表示将设备的视频流同时转发到指定的目的地.
+    @Schema(description = "推流目的地", example = "rtsp://127.0.0.1/live/stream?token=xxx")
     public String getDestination() {
         return getOrNull("destination", String.class);
     }
@@ -38,6 +46,7 @@ public class StartPushStreamingCommand extends AbstractCommand<Mono<MediaStreamI
         return with("destination", destination);
     }
 
+    @Schema(description = "SDP信息")
     public String getSdp() {
         return getOrNull("sdp", String.class);
     }
@@ -47,7 +56,7 @@ public class StartPushStreamingCommand extends AbstractCommand<Mono<MediaStreamI
     }
 
     // 录像相关参数
-
+    @Schema(description = "推送录像视频流")
     public boolean isForRecord() {
         return CastUtils.castBoolean(readable().get("forRecord"));
     }
@@ -56,6 +65,7 @@ public class StartPushStreamingCommand extends AbstractCommand<Mono<MediaStreamI
         return with("forRecord", forRecord);
     }
 
+    @Schema(description = "录像开始时间")
     public Long getStartWith() {
         return getOrNull("startWith", Long.class);
     }
@@ -64,6 +74,7 @@ public class StartPushStreamingCommand extends AbstractCommand<Mono<MediaStreamI
         return with("startWith", startWith);
     }
 
+    @Schema(description = "录像结束时间")
     public Long getEndWith() {
         return getOrNull("endWith", Long.class);
     }
@@ -72,12 +83,17 @@ public class StartPushStreamingCommand extends AbstractCommand<Mono<MediaStreamI
         return with("endWith", endWith);
     }
 
+    @Schema(description = "录像推流速度")
     public Integer getDownloadSpeed() {
         return getOrNull("downloadSpeed", Integer.class);
     }
 
     public StartPushStreamingCommand setDownloadSpeed(Integer downloadSpeed) {
         return with("downloadSpeed", downloadSpeed);
+    }
+
+    public static FunctionMetadata metadata() {
+        return CommandMetadataResolver.resolve(StartPushStreamingCommand.class);
     }
 
 }
