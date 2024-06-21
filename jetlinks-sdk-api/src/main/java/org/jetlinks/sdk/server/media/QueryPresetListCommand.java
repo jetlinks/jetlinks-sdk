5e85cc5fe7be39cc81e7d47225849285c8bc6f89
zhouhao <zh.sqy@qq.com>
2024-06-21 09:52:32 +0800

    refactor: 优化命令

--- jetlinks-sdk-api/src/main/java/org/jetlinks/sdk/server/media/QueryPresetListCommand.java
+++ jetlinks-sdk-api/src/main/java/org/jetlinks/sdk/server/media/QueryPresetListCommand.java
@@ -1,17 +1,19 @@
 package org.jetlinks.sdk.server.media;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import org.jetlinks.core.command.AbstractCommand;
 import org.jetlinks.core.command.CommandMetadataResolver;
 import org.jetlinks.core.metadata.FunctionMetadata;
 import org.jetlinks.sdk.server.commons.cmd.QueryListCommand;
 import org.springframework.core.ResolvableType;
+import reactor.core.publisher.Flux;
 
 /**
  * 查询预置位.
  *
  * @author zhangji 2024/6/16
  */
-public class QueryPresetListCommand extends QueryListCommand<MediaPreset> {
+public class QueryPresetListCommand extends AbstractCommand<Flux<MediaPreset>,QueryPresetListCommand> {
 
     @Schema(description = "设备ID")
     public String getDeviceId() {
