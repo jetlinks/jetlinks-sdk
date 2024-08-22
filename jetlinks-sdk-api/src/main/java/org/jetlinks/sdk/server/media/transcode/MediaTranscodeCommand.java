666672dc491ad1cae6fde0317761e452d80e114f
zhouhao <zh.sqy@qq.com>
2024-08-22 18:53:24 +0800

    优化

--- jetlinks-sdk-api/src/main/java/org/jetlinks/sdk/server/media/transcode/MediaTranscodeCommand.java
+++ jetlinks-sdk-api/src/main/java/org/jetlinks/sdk/server/media/transcode/MediaTranscodeCommand.java
@@ -1,6 +1,5 @@
 package org.jetlinks.sdk.server.media.transcode;
 
-import org.jetlinks.sdk.server.utils.CastUtils;
 
 public class MediaTranscodeCommand extends TranscodeCommand<MediaTranscodeCommand> {
 
--- jetlinks-sdk-api/src/main/java/org/jetlinks/sdk/server/media/transcode/VideoCodecs.java
+++ jetlinks-sdk-api/src/main/java/org/jetlinks/sdk/server/media/transcode/VideoCodecs.java
@@ -1,7 +1,7 @@
 package org.jetlinks.sdk.server.media.transcode;
 
 public interface VideoCodecs {
-
+    String copy = "copy";
     String H264 = "h264";
     String H265 = "h265";
     String MPEG4 = "mpeg4";
