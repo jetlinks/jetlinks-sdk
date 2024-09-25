45ab5922e366f38b92be29bb733d5726870aee41
gyl <1554863190@qq.com>
2024-09-25 10:30:59 +0800

    feat: 添加预存储文件信息相关命令及参数

---
+++ jetlinks-sdk-api/src/main/java/org/jetlinks/sdk/server/file/PrepareUploadRequest.java
@@ -0,0 +1,44 @@
+package org.jetlinks.sdk.server.file;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.jetlinks.core.utils.SerializeUtils;
+
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+
+/**
+ * @author gyl
+ * @since 2.3
+ */
+@Getter
+@Setter
+public class PrepareUploadRequest implements Externalizable {
+    private static final long serialVersionUID = 1L;
+    private String name;
+    private long length;
+    private String md5;
+    private String sha256;
+    private long createTime = System.currentTimeMillis();
+
+    @Override
+    public void writeExternal(ObjectOutput out) throws IOException {
+        out.writeUTF(name);
+        out.writeLong(length);
+        SerializeUtils.writeNullableUTF(md5, out);
+        SerializeUtils.writeNullableUTF(sha256, out);
+        out.writeLong(createTime);
+    }
+
+    @Override
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+        name = in.readUTF();
+        length = in.readLong();
+        md5 = SerializeUtils.readNullableUTF(in);
+        sha256 = SerializeUtils.readNullableUTF(in);
+        createTime = in.readLong();
+
+    }
+}
