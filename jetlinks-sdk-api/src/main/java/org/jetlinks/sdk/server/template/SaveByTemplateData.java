ff8385bae399a111f558cfad365119350892062f
gyl <1554863190@qq.com>
2024-10-10 16:01:01 +0800

    refactor: 优化根据模板保存数据命令响应体

--- jetlinks-sdk-api/src/main/java/org/jetlinks/sdk/server/template/SaveByTemplateData.java
+++ jetlinks-sdk-api/src/main/java/org/jetlinks/sdk/server/template/SaveByTemplateData.java
@@ -20,8 +20,30 @@ import java.io.ObjectOutput;
 public class SaveByTemplateData implements Externalizable {
     @Schema(title = "id")
     private String id;
+    @Schema(title = "是否成功")
+    private boolean success;
     @Schema(title = "数据")
     private Object date;
+    @Schema(title = "错误信息")
+    private String errorMessage;
+
+
+    public static SaveByTemplateData success(String id, Object date) {
+        SaveByTemplateData data = new SaveByTemplateData();
+        data.setSuccess(true);
+        data.setId(id);
+        data.setDate(date);
+        return data;
+    }
+
+
+    public static SaveByTemplateData error(String id, String errorMessage) {
+        SaveByTemplateData data = new SaveByTemplateData();
+        data.setSuccess(false);
+        data.setId(id);
+        data.setErrorMessage(errorMessage);
+        return data;
+    }
 
 
     public <T> T dateTo(Class<T> clazz) {
@@ -31,12 +53,16 @@ public class SaveByTemplateData implements Externalizable {
     @Override
     public void writeExternal(ObjectOutput out) throws IOException {
         out.writeUTF(id);
+        out.writeBoolean(success);
+        SerializeUtils.writeNullableUTF(errorMessage, out);
         SerializeUtils.writeObject(SerializeUtils.convertToSafelySerializable(date), out);
     }
 
     @Override
     public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
-        id = SerializeUtils.readNullableUTF(in);
+        id = in.readUTF();
+        success = in.readBoolean();
+        errorMessage = SerializeUtils.readNullableUTF(in);
         date = SerializeUtils.readObject(in);
     }
 }
