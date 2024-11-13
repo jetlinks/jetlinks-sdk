fd9c1f9abfa8caf55766a46b72ce9fb7005af922
fighter-wang <11291691+fighter-wang@user.noreply.gitee.com>
2024-11-13 18:46:03 +0800

    fix(sdk): 代码优化

--- jetlinks-sdk-api/src/main/java/org/jetlinks/sdk/server/device/cmd/UnbindChildDeviceCommand.java
+++ jetlinks-sdk-api/src/main/java/org/jetlinks/sdk/server/device/cmd/UnbindChildDeviceCommand.java
@@ -4,10 +4,9 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import org.jetlinks.core.command.AbstractCommand;
 import org.jetlinks.core.command.CommandHandler;
 import org.jetlinks.core.command.CommandMetadataResolver;
-import org.jetlinks.core.command.CommandUtils;
-import org.jetlinks.core.metadata.SimpleFunctionMetadata;
+import org.jetlinks.core.metadata.FunctionMetadata;
+import org.jetlinks.sdk.server.file.PrepareUploadCommand;
 import org.jetlinks.sdk.server.utils.ConverterUtils;
-import org.springframework.core.ResolvableType;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
@@ -44,17 +43,14 @@ public class UnbindChildDeviceCommand extends AbstractCommand<Mono<Void>, Unbind
 
     public static CommandHandler<UnbindChildDeviceCommand, Mono<Void>> createHandler(Function<UnbindChildDeviceCommand, Mono<Void>> handler) {
         return CommandHandler.of(
-            () -> {
-                SimpleFunctionMetadata metadata = new SimpleFunctionMetadata();
-                metadata.setId(CommandUtils.getCommandIdByType(UnbindChildDeviceCommand.class));
-                metadata.setName("解绑子设备");
-                metadata.setDescription("解绑子设备");
-                metadata.setInputs(CommandMetadataResolver.resolveInputs(ResolvableType.forClass(UnbindChildDeviceCommand.class)));
-                return metadata;
-            },
+            UnbindChildDeviceCommand::metadata,
             (cmd, ignore) -> handler.apply(cmd),
             UnbindChildDeviceCommand::new
         );
 
+    }
+
+    public static FunctionMetadata metadata() {
+        return CommandMetadataResolver.resolve(PrepareUploadCommand.class);
     }
 }
