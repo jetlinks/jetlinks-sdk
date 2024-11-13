d28641047089c3c93a0321fb64f5053f2f2cdd8d
fighter-wang <11291691+fighter-wang@user.noreply.gitee.com>
2024-11-13 18:27:40 +0800

    fix(sdk): 增加Schema注解

--- jetlinks-sdk-api/src/main/java/org/jetlinks/sdk/server/device/cmd/UnbindChildDeviceCommand.java
+++ jetlinks-sdk-api/src/main/java/org/jetlinks/sdk/server/device/cmd/UnbindChildDeviceCommand.java
@@ -1,30 +1,29 @@
 package org.jetlinks.sdk.server.device.cmd;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.jetlinks.core.command.AbstractCommand;
 import org.jetlinks.core.command.CommandHandler;
 import org.jetlinks.core.command.CommandMetadataResolver;
 import org.jetlinks.core.command.CommandUtils;
 import org.jetlinks.core.metadata.SimpleFunctionMetadata;
-import org.jetlinks.core.metadata.SimplePropertyMetadata;
-import org.jetlinks.core.metadata.types.ArrayType;
-import org.jetlinks.core.metadata.types.StringType;
 import org.jetlinks.sdk.server.utils.ConverterUtils;
 import org.springframework.core.ResolvableType;
 import reactor.core.publisher.Mono;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.function.Function;
 
 /**
  * @author wangsheng
  */
+@Schema(title = "解除子设备绑定")
 public class UnbindChildDeviceCommand extends AbstractCommand<Mono<Void>, UnbindChildDeviceCommand> {
 
     public static final String PARENT_ID = "parentId";
 
     public static final String DEVICE_IDS = "deviceIds";
 
+    @Schema(description = "父设备ID")
     public String  getParentId(){
         return getOrNull("parentId", String.class);
     }
@@ -33,6 +32,7 @@ public class UnbindChildDeviceCommand extends AbstractCommand<Mono<Void>, Unbind
         return with(PARENT_ID, parentId);
     }
 
+    @Schema(description = "设备ID集合")
     public List<String> getDeviceIds(){
         return ConverterUtils
             .convertToList(readable().get(DEVICE_IDS), String::valueOf);
@@ -49,7 +49,7 @@ public class UnbindChildDeviceCommand extends AbstractCommand<Mono<Void>, Unbind
                 metadata.setId(CommandUtils.getCommandIdByType(UnbindChildDeviceCommand.class));
                 metadata.setName("解绑子设备");
                 metadata.setDescription("解绑子设备");
-                metadata.setInputs(CommandMetadataResolver.resolveInputs(ResolvableType.forType(UnbindChildDeviceCommand.class)));
+                metadata.setInputs(CommandMetadataResolver.resolveInputs(ResolvableType.forClass(UnbindChildDeviceCommand.class)));
                 return metadata;
             },
             (cmd, ignore) -> handler.apply(cmd),
