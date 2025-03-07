86bad67e80a2a8eb4bf376e4cd2228c882a99103
EddyChao <74349287+EddyChao@users.noreply.github.com>
2025-03-07 14:58:20 +0800

    feat(sdk): 添加设备消息订阅命令； (#76)
    * feat(sdk): 添加设备消息订阅命令；
    
    * feat(sdk): 添加设备消息订阅命令；
    
    * feat(可视化): 修改命令名称以及描述信息；
    
    * feat(sdk): 移除历史数据参数；

---
+++ jetlinks-sdk-api/src/main/java/org/jetlinks/sdk/server/device/cmd/SubscribeDevicePropertyCommand.java
@@ -0,0 +1,44 @@
+package org.jetlinks.sdk.server.device.cmd;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import org.jetlinks.core.command.AbstractCommand;
+import org.jetlinks.core.command.CommandMetadataResolver;
+import org.jetlinks.core.metadata.FunctionMetadata;
+import org.jetlinks.sdk.server.commons.cmd.UnboundedResponseCommand;
+import org.jetlinks.sdk.server.device.DeviceProperty;
+import org.jetlinks.sdk.server.utils.ConverterUtils;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
+@Schema(title = "订阅设备属性数据", description = "根据参数设备的实时属性数据")
+public class SubscribeDevicePropertyCommand extends AbstractCommand<Flux<DeviceProperty>, SubscribeDevicePropertyCommand>
+    implements UnboundedResponseCommand<DeviceProperty> {
+
+    public static final String DEVICE_IDS = "deviceIds";
+    public static final String PRODUCT_ID = "productId";
+
+    @Schema(title = "设备ID集合")
+    public List<String> getDeviceIds() {
+        return ConverterUtils
+            .convertToList(readable().get(DEVICE_IDS), String::valueOf);
+    }
+
+    public SubscribeDevicePropertyCommand setDeviceIds(List<String> deviceIds) {
+        return with(DEVICE_IDS, deviceIds);
+    }
+
+    public static FunctionMetadata metadata() {
+        return CommandMetadataResolver
+            .resolve(SubscribeDevicePropertyCommand.class);
+    }
+
+    @Schema(title = "产品ID")
+    public String getProductId() {
+        return String.valueOf(readable().get(PRODUCT_ID));
+    }
+
+    public SubscribeDevicePropertyCommand setProductId(String productId) {
+        return with(PRODUCT_ID, productId);
+    }
+}
