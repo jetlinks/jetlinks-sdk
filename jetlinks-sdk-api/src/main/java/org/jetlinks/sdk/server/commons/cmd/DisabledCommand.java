3e0e01f50676e52df7d880ae2e34f7a7df55eda6
EddyChao <1403774697@qq.com>
2025-03-14 10:39:27 +0800

    refactor(sdk): 修改获取命令metadata方式为注解方式；

--- jetlinks-sdk-api/src/main/java/org/jetlinks/sdk/server/commons/cmd/DisabledCommand.java
+++ jetlinks-sdk-api/src/main/java/org/jetlinks/sdk/server/commons/cmd/DisabledCommand.java
@@ -1,21 +1,20 @@
 package org.jetlinks.sdk.server.commons.cmd;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import org.hswebframework.ezorm.core.CastUtil;
 import org.jetlinks.core.command.CommandHandler;
-import org.jetlinks.core.command.CommandUtils;
+import org.jetlinks.core.command.CommandMetadataResolver;
 import org.jetlinks.core.metadata.FunctionMetadata;
 import org.jetlinks.core.metadata.SimpleFunctionMetadata;
-import org.jetlinks.core.metadata.SimplePropertyMetadata;
-import org.jetlinks.core.metadata.types.ArrayType;
-import org.jetlinks.core.metadata.types.StringType;
 import reactor.core.publisher.Mono;
 
-import java.util.Collections;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
 /**
  * @author liusq
  */
+@Schema(title = "禁用", description = "禁用或注销")
 public class DisabledCommand extends EnabledCommand {
 
     public static CommandHandler<EnabledCommand, Mono<Void>> createHandler(Function<EnabledCommand, Mono<Void>> handler) {
@@ -39,15 +38,8 @@ public class DisabledCommand extends EnabledCommand {
     }
 
     public static FunctionMetadata metadata(Consumer<SimpleFunctionMetadata> custom) {
-        SimpleFunctionMetadata metadata = new SimpleFunctionMetadata();
-        metadata.setId(CommandUtils.getCommandIdByType(DisabledCommand.class));
-        metadata.setName("禁用");
-        metadata.setDescription("禁用或注销");
-        metadata.setInputs(Collections.singletonList(
-            SimplePropertyMetadata
-                .of("idList", "id数组",
-                    new ArrayType().elementType(StringType.GLOBAL))));
-        custom.accept(metadata);
+        FunctionMetadata metadata = CommandMetadataResolver.resolve(DisabledCommand.class);
+        custom.accept(CastUtil.cast(metadata));
         return metadata;
     }
 }
--- jetlinks-sdk-api/src/main/java/org/jetlinks/sdk/server/commons/cmd/EnabledCommand.java
+++ jetlinks-sdk-api/src/main/java/org/jetlinks/sdk/server/commons/cmd/EnabledCommand.java
@@ -1,17 +1,15 @@
 package org.jetlinks.sdk.server.commons.cmd;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.apache.commons.collections4.CollectionUtils;
+import org.hswebframework.ezorm.core.CastUtil;
 import org.jetlinks.core.command.CommandHandler;
-import org.jetlinks.core.command.CommandUtils;
+import org.jetlinks.core.command.CommandMetadataResolver;
 import org.jetlinks.core.metadata.FunctionMetadata;
 import org.jetlinks.core.metadata.SimpleFunctionMetadata;
-import org.jetlinks.core.metadata.SimplePropertyMetadata;
-import org.jetlinks.core.metadata.types.ArrayType;
-import org.jetlinks.core.metadata.types.StringType;
 import org.jetlinks.sdk.server.utils.ConverterUtils;
 import reactor.core.publisher.Mono;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -19,6 +17,7 @@ import java.util.function.Function;
 /**
  * @author liusq
  */
+@Schema(title = "启用", description = "启用或激活")
 public class EnabledCommand extends OperationByIdCommand<Mono<Void>, EnabledCommand> {
     public static final String PARAMETER_KEY = "idList";
 
@@ -48,14 +47,8 @@ public class EnabledCommand extends OperationByIdCommand<Mono<Void>, EnabledComm
     }
 
     public static FunctionMetadata metadata(Consumer<SimpleFunctionMetadata> custom) {
-        SimpleFunctionMetadata metadata = new SimpleFunctionMetadata();
-        metadata.setId(CommandUtils.getCommandIdByType(EnabledCommand.class));
-        metadata.setName("启用");
-        metadata.setDescription("启用或激活");
-        metadata.setInputs(Collections.singletonList(SimplePropertyMetadata
-            .of("idList", "id数组",
-                new ArrayType().elementType(StringType.GLOBAL))));
-        custom.accept(metadata);
+        FunctionMetadata metadata = CommandMetadataResolver.resolve(EnabledCommand.class);
+        custom.accept(CastUtil.cast(metadata));
         return metadata;
     }
 
