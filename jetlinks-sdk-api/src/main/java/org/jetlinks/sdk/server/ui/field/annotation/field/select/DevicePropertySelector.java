5d090bba4397343f366082b61e21e6c5afca8944
gyl <80454341+goten7@users.noreply.github.com>
2025-03-21 13:37:39 +0800

    feat: 添加属性选择器；订阅设备属性支持选择属性 (#81)

---
+++ jetlinks-sdk-api/src/main/java/org/jetlinks/sdk/server/ui/field/annotation/field/select/DevicePropertySelector.java
@@ -0,0 +1,44 @@
+package org.jetlinks.sdk.server.ui.field.annotation.field.select;
+
+import org.jetlinks.core.annotation.ui.Selector;
+import org.springframework.core.annotation.AliasFor;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+
+/**
+ * 设备属性选择器
+ */
+@Target({FIELD, METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Selector(type = "device-property")
+public @interface DevicePropertySelector {
+
+    @AliasFor(annotation = Selector.class)
+    boolean multiple() default false;
+
+    /**
+     * 指定设备id取值
+     */
+    String deviceId() default "";
+
+    /**
+     * 指定产品id取值
+     */
+    String productId() default "";
+
+    /**
+     * 指定设备id取值来源
+     */
+    String deviceIdKey() default "";
+
+    /**
+     * 指定产品id取值来源
+     */
+    String productIdKey() default "";
+
+}
