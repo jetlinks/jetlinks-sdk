28cd37cece3058e84abeacf5ef6182ad4eaf1bec
“kwj” <1278361885@qq.com>
2025-07-21 18:14:50 +0800

    feat(sdk): 设备信息字段描述

--- jetlinks-sdk-api/src/main/java/org/jetlinks/sdk/server/device/DeviceInfo.java
+++ jetlinks-sdk-api/src/main/java/org/jetlinks/sdk/server/device/DeviceInfo.java
@@ -1,6 +1,7 @@
 package org.jetlinks.sdk.server.device;
 
 import com.alibaba.fastjson.JSONObject;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 import org.jetlinks.core.metadata.Jsonable;
@@ -15,26 +16,37 @@ import java.util.Map;
 public class DeviceInfo implements Externalizable, Jsonable {
     private static final long serialVersionUID = 1L;
 
+    @Schema(title = "设备ID")
     private String id;
 
+    @Schema(title = "设备名称")
     private String name;
 
+    @Schema(title = "产品ID")
     private String productId;
 
+    @Schema(title = "产品名称")
     private String productName;
 
+    @Schema(title = "图片地址")
     private String photoUrl;
 
+    @Schema(title = "配置信息")
     private Map<String, Object> configuration;
 
+    @Schema(title = "创建人ID")
     private String creatorId;
 
+    @Schema(title = "创建时间")
     private Long createTime;
 
+    @Schema(title = "设备状态")
     private DeviceState state;
 
+    @Schema(title = "父设备ID")
     private String parentId;
 
+    @Schema(title = "描述")
     private String describe;
 
     @Override
