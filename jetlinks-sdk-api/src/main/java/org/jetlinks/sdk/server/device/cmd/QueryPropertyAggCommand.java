0f085954191d8b604d5c4d914f7f159b6aeab756
gyl <1554863190@qq.com>
2025-10-15 18:42:42 +0800

    feat: 优化属性集合查询命令

--- jetlinks-sdk-api/src/main/java/org/jetlinks/sdk/server/device/cmd/QueryPropertyAggCommand.java
+++ jetlinks-sdk-api/src/main/java/org/jetlinks/sdk/server/device/cmd/QueryPropertyAggCommand.java
@@ -1,6 +1,9 @@
 package org.jetlinks.sdk.server.device.cmd;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
 import org.jetlinks.core.command.CommandHandler;
 import org.jetlinks.core.command.CommandMetadataResolver;
 import org.jetlinks.core.metadata.FunctionMetadata;
@@ -13,15 +16,13 @@ import org.jetlinks.core.metadata.types.StringType;
 import org.jetlinks.sdk.server.commons.AggregationRequest;
 import org.jetlinks.sdk.server.commons.cmd.OperationByIdCommand;
 import org.jetlinks.sdk.server.device.DevicePropertyAggregation;
+import org.jetlinks.sdk.server.ui.field.annotation.field.select.DevicePropertySelector;
 import org.jetlinks.sdk.server.ui.field.annotation.field.select.DeviceSelector;
 import org.springframework.core.ResolvableType;
 import reactor.core.publisher.Flux;
 
-import jakarta.validation.constraints.NotBlank;
 import java.lang.reflect.Type;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Function;
 
 /**
@@ -29,7 +30,7 @@ import java.util.function.Function;
  *
  * @author zhangji 2024/1/16
  */
-@Schema(title = "聚合查询设备属性")
+@Schema(title = "聚合查询设备属性",description = "聚合查询设备属性数据，例如求和、求平均值等")
 public class QueryPropertyAggCommand extends OperationByIdCommand<Flux<Map<String, Object>>, QueryPropertyAggCommand> {
 
     private static final long serialVersionUID = 1L;
@@ -74,6 +75,35 @@ public class QueryPropertyAggCommand extends OperationByIdCommand<Flux<Map<Strin
         return this;
     }
 
+    @Schema(title = "简单聚合参数", description = "进行简单聚合时使用，此值为空时才使用其余参数进行聚合")
+    @NotBlank
+    public SimpleAggParams getSimple() {
+        return getOrNull("simple", SimpleAggParams.class);
+    }
+
+    public QueryPropertyAggCommand withSimple(SimpleAggParams simple) {
+        writable().put("simple", simple);
+        return this;
+    }
+
+    public void generate() {
+        SimpleAggParams simple = getSimple();
+        if (simple != null) {
+            withId(simple.getDeviceId());
+
+            AggregationRequest request = new AggregationRequest();
+            request.setFrom(new Date(simple.getFrom()));
+            request.setTo(new Date(simple.getTo()));
+            withQuery(request);
+
+            DevicePropertyAggregation aggregation = new DevicePropertyAggregation();
+            aggregation.setProperty(simple.getProperty());
+            aggregation.setAgg(simple.getAgg());
+            withColumns(Collections.singletonList(aggregation));
+        }
+    }
+
+
     public static CommandHandler<QueryPropertyAggCommand, Flux<Map<String, Object>>> createHandler(
         Function<QueryPropertyAggCommand, Flux<Map<String, Object>>> handler
     ) {
@@ -113,6 +143,29 @@ public class QueryPropertyAggCommand extends OperationByIdCommand<Flux<Map<Strin
 
     public static FunctionMetadata metadata() {
         return CommandMetadataResolver.resolve(QueryPropertyAggCommand.class);
+    }
+
+
+    @Getter
+    @Setter
+    public static class SimpleAggParams {
+
+        @DeviceSelector
+        @Schema(title = "设备id")
+        private String deviceId;
+
+        @DevicePropertySelector(deviceIdKey = "id")
+        @Schema(title = "属性id", description = "设备物模型属性id")
+        private String property;
+
+        @Schema(title = "聚合方式", description = "例如count(计数),sum(求和),max(最大值),min(最小值),avg(平均值)等")
+        private String agg;
+
+        @Schema(title = "开始时间", description = "毫秒时间戳")
+        private Long from;
+
+        @Schema(title = "结束时间", description = "毫秒时间戳")
+        private Long to;
     }
 
 }
