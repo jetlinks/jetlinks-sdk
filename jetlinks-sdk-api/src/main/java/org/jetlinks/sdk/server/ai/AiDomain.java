39394b0aaf895416a7ca10233b4e366cac519834
gyl <1554863190@qq.com>
2026-03-25 16:27:05 +0800

    feat: 支持根据Ai维度获取simple目标实现类

--- jetlinks-sdk-api/src/main/java/org/jetlinks/sdk/server/ai/AiDomain.java
+++ jetlinks-sdk-api/src/main/java/org/jetlinks/sdk/server/ai/AiDomain.java
@@ -11,13 +11,32 @@ import org.hswebframework.web.dict.I18nEnumDict;
 @Getter
 public enum AiDomain implements I18nEnumDict<String> {
 
-    ComputerVision("计算机视觉"),
+    ComputerVision("计算机视觉") {
+
+        @Override
+        public TaskTarget createSimpleTaskTarget() {
+            return new SimpleComputerVisionTaskTarget();
+        }
+
+        @Override
+        public TaskTarget createSimpleTaskTarget(String value, String text, String description) {
+            return SimpleComputerVisionTaskTarget.of(value, text, description);
+        }
+    },
     // 基于大语言模型的智能体
     LLMAgent("智能体"),
     // 大语言模型
     LLM("大语言模型");
 
     private final String text;
+
+    public TaskTarget createSimpleTaskTarget() {
+        return new SimpleTaskTarget();
+    }
+
+    public TaskTarget createSimpleTaskTarget(String value, String text, String description) {
+        return SimpleTaskTarget.of(value, text, description);
+    }
 
 
     @Override
