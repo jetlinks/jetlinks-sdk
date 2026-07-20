184dee65559c71a2956b78ab3120edec994330a1
gyl <1554863190@qq.com>
2026-07-20 18:28:20 +0800

    feat(AI视觉): 支持图片二进制数据处理

--- jetlinks-sdk-api/src/main/java/org/jetlinks/sdk/server/media/service/AiProcessImageCommand.java
+++ jetlinks-sdk-api/src/main/java/org/jetlinks/sdk/server/media/service/AiProcessImageCommand.java
@@ -1,22 +1,33 @@
 package org.jetlinks.sdk.server.media.service;
 
+import io.netty.buffer.ByteBuf;
 import io.swagger.v3.oas.annotations.media.Schema;
-import org.jetlinks.core.annotation.command.CommandHandler;
 import org.jetlinks.core.command.AbstractCommand;
 import reactor.core.publisher.Mono;
 
 import java.util.Map;
 
-public class AiProcessImageCommand extends AbstractCommand<Mono<Map<String,Object>>, AiProcessImageCommand> {
+public class AiProcessImageCommand extends AbstractCommand<Mono<Map<String, Object>>, AiProcessImageCommand> {
+
+    public static final String CONTENT_KEY = "content";
+    public static final String FILE_URL_KEY = "fileUrl";
+
+    @Schema(title = "图片数据")
+    public ByteBuf getContent() {
+        Object image = readable().get(CONTENT_KEY);
+        return image instanceof ByteBuf ? (ByteBuf) image : null;
+    }
+
+    public AiProcessImageCommand setContent(ByteBuf image) {
+        return with(CONTENT_KEY, image);
+    }
 
     @Schema(title = "文件id")
     public String getFileUrl() {
-        return getOrNull("fileUrl", String.class);
+        return getOrNull(FILE_URL_KEY, String.class);
     }
 
     public AiProcessImageCommand setFileUrl(String fileUrl) {
-        with("fileUrl", fileUrl);
-        return this;
+        return with(FILE_URL_KEY, fileUrl);
     }
 }
-
