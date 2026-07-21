e360f8f6a71128347ef311a074ee96b7f220c909
gyl <80454341+goten7@users.noreply.github.com>
2026-07-21 14:30:28 +0800

    feat(AI视觉): 支持图片二进制数据处理 (#135)
    * feat(AI视觉): 支持图片二进制数据处理
    
    * feat: 优化
    
    * feat: 优化

--- jetlinks-sdk-api/src/main/java/org/jetlinks/sdk/server/media/service/AiProcessImageCommand.java
+++ jetlinks-sdk-api/src/main/java/org/jetlinks/sdk/server/media/service/AiProcessImageCommand.java
@@ -1,22 +1,27 @@
 package org.jetlinks.sdk.server.media.service;
 
+import io.netty.buffer.ByteBuf;
 import io.swagger.v3.oas.annotations.media.Schema;
-import org.jetlinks.core.annotation.command.CommandHandler;
-import org.jetlinks.core.command.AbstractCommand;
-import reactor.core.publisher.Mono;
+import org.jetlinks.core.command.AbstractStreamCommand;
+import org.jetlinks.core.utils.ConverterUtils;
 
 import java.util.Map;
 
-public class AiProcessImageCommand extends AbstractCommand<Mono<Map<String,Object>>, AiProcessImageCommand> {
+public class AiProcessImageCommand extends AbstractStreamCommand<ByteBuf, Map<String, Object>, AiProcessImageCommand> {
+
+    public static final String FILE_URL_KEY = "fileUrl";
 
     @Schema(title = "文件id")
     public String getFileUrl() {
-        return getOrNull("fileUrl", String.class);
+        return getOrNull(FILE_URL_KEY, String.class);
     }
 
     public AiProcessImageCommand setFileUrl(String fileUrl) {
-        with("fileUrl", fileUrl);
-        return this;
+        return with(FILE_URL_KEY, fileUrl);
+    }
+
+    @Override
+    public ByteBuf convertStreamValue(Object value) {
+        return ConverterUtils.convertNettyBuffer(value);
     }
 }
-
