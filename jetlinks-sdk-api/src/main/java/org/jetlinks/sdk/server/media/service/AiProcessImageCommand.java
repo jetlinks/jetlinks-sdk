19c057f6019923da31b0dda6c3ae9d45a50c6349
gyl <1554863190@qq.com>
2026-07-21 10:26:12 +0800

    feat: 优化

--- jetlinks-sdk-api/src/main/java/org/jetlinks/sdk/server/media/service/AiProcessImageCommand.java
+++ jetlinks-sdk-api/src/main/java/org/jetlinks/sdk/server/media/service/AiProcessImageCommand.java
@@ -2,25 +2,13 @@ package org.jetlinks.sdk.server.media.service;
 
 import io.netty.buffer.ByteBuf;
 import io.swagger.v3.oas.annotations.media.Schema;
-import org.jetlinks.core.command.AbstractCommand;
-import reactor.core.publisher.Mono;
+import org.jetlinks.core.command.AbstractStreamCommand;
 
 import java.util.Map;
 
-public class AiProcessImageCommand extends AbstractCommand<Mono<Map<String, Object>>, AiProcessImageCommand> {
+public class AiProcessImageCommand extends AbstractStreamCommand<ByteBuf, Map<String, Object>, AiProcessImageCommand> {
 
-    public static final String CONTENT_KEY = "content";
     public static final String FILE_URL_KEY = "fileUrl";
-
-    @Schema(title = "图片数据")
-    public ByteBuf getContent() {
-        Object image = readable().get(CONTENT_KEY);
-        return image instanceof ByteBuf ? (ByteBuf) image : null;
-    }
-
-    public AiProcessImageCommand setContent(ByteBuf image) {
-        return with(CONTENT_KEY, image);
-    }
 
     @Schema(title = "文件id")
     public String getFileUrl() {
